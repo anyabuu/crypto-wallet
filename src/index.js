b6a1c830c5754b968763aaf782310e807ae3abc7
Anya <81263768+anyabuu@users.noreply.github.com>
2024-09-01 18:20:35 +0300

    adaptive made

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import './scss/style.scss';
+
+const menuBtn = document.querySelector('.header__menu-bar');
+const menu = document.querySelector('.header__burger');
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  if (menuBtn) {
+    menuBtn.addEventListener('click', function () {
+      menu.classList.toggle('active');
+      menuBtn.classList.toggle('active');
+    });
+  }
+})
